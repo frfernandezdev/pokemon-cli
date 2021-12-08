0a102fed7863cd74e3e46b4920d69a7db51436f8
Fernando Fernandez <frfernandezdev@gmail.com>
2021-12-08 09:19:45 -0400

    feat(core): added Spinner

---
+++ src/modules/core/infrastructure/Spinner.py
@@ -0,0 +1,11 @@
+from halo import Halo
+
+class Spinner:
+    def __init__(self):
+        self.__spinner = Halo("Loading...")
+
+    def start(self):
+        self.__spinner.start()
+
+    def stop(self):
+        self.__spinner.stop()
