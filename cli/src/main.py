d3513656da515c611f90e4ecfaadcd8e0b46324d
Fernando Fernandez <frfernandezdev@gmail.com>
2021-12-11 15:59:00 -0400

    feat(cli): added connection with rabbt-mq

--- cli/src/main.py
+++ cli/src/main.py
@@ -1,4 +1,16 @@
+import pika
 from modules.core.application import PokemonCLI
+
+
+def rabbit_mq(self):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
+    channel = connection.channel()
+    channel.queue_declare(queue="hello")
+    channel.basic_publish(exchange="", routing_key="hello", body="Hello World!")
+
+    print("[X] Send 'Hello World!'")
+
+    connection.close()
 
 
 def main():
